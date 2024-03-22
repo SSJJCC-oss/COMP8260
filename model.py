632124a15fb947230d89937a7b0c259545732413
SSJJCC-oss <74069782+SSJJCC-oss@users.noreply.github.com>
2024-03-22 16:23:46 +0000

    model change

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim #optimization
+import torch.nn.functional as F #activation function
+import os
+
+class Linear_QNet(nn.Module):
+    #initialises 3 layers
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x)) #relu activation on the first layer
+        x = self.linear2(x) 
+        return x #returns the output of the second layer
+
+    def save(self, file_name='model.pth'): #saving the file
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr) #optimizer Adam
+        self.criterion = nn.MSELoss() #Mean Square Error
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+       
